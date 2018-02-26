afcd9cd671896b7c957aed29b6e2f5dea27fbf18
michaelbaud <michael.baudoin.7@gmail.com>
2018-02-26 13:58:46 +0100

    premier commit

---
+++ main.js
@@ -0,0 +1,29 @@
+$(function(){
+  var aucune = $('option[value="aucune"]');
+  var vert = $('option[value="vert"]');
+  var jaune = $('option[value="jaune"]');
+  var rouge = $('option[value="rouge"]');
+  
+  var box = $('.box');
+  
+  aucune.click(function(){
+    box.css({
+      'background-color' : 'none'
+    });
+  })
+  vert.click(function(){
+    box.css({
+      'background-color' : 'green'
+    });
+  })
+  jaune.click(function(){
+    box.css({
+      'background-color' : 'yellow'
+    });
+  })
+  rouge.click(function(){
+    box.css({
+      'background-color' : 'red'
+    });
+  })
+});
